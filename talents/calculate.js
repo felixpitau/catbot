f115e16cf62bc1570243b8b28a330fb671ea876c
Austin "Felix" Lee <felixpitau@gmail.com>
2018-03-06 15:55:21 -0700

    Add calculate talent

---
+++ talents/calculate.js
@@ -0,0 +1,38 @@
+import Talent from '../talent'
+import math from 'mathjs'
+
+export default class Calculate extends Talent {
+
+  parseDice (text) {
+    let rollPat = /\d*d\d+/gi;
+    let rollArr = [];
+    let rollMatches = text.match(rollPat);
+    for (let i = 0; i < rollMatches.length; i++) {
+      let roll = rollMatches[i];
+      let numPat = /(\d*)d(\d+)/gi;
+      let num = numPat.exec(roll);
+      let rollVal = 0;
+      let rollCount = parseInt(num[1]);
+      if (num[1] == "")
+        rollCount = 1;
+      for (let j = 0; j < rollCount; j++) {
+        rollVal += Math.ceil(Math.random() * parseInt(num[2]));
+      }
+      text = text.replace(/\d*d\d+/i, rollVal);
+    }
+    return text;
+  }
+
+  onMessage (message) {
+    if (!this.isFromSelf(message)) {
+      if (message.content.match(/^(calculate|calc|roll) (.+)/gi) !== null) {
+        let calcPat = /^(calculate|calc) (.+)/gi;
+        let rollPat = /(\d*d\d+)/gi;
+        let calcArr = calcPat.exec(message.content);
+        let parsable = calcArr[2];
+        let rollArr = rollPat.exec(parsable);
+        message.channel.send('📊 ' + parser.eval(this.parseDice(parsable)));
+      }
+    }
+  }
+}
