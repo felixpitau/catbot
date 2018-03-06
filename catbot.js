5271354c2d362851e92babd32f014ff2e6a25a9a
Austin "Felix" Lee <felixpitau@gmail.com>
2018-03-06 15:55:07 -0700

    Add basic functionality

---
+++ catbot.js
@@ -0,0 +1,19 @@
+require("babel-register");
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const fs = require('fs')
+const id = require('./mem/id')
+
+const talents = require('./talent').talents
+
+client.on('ready', () => {
+  console.log('Cat bot is ready!')
+})
+
+client.on('message', message => {
+  for (let talent in talents) {
+    talent.onMessage(message)
+  }
+))
+
+client.login(id.login)
