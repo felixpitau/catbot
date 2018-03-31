2f729087d4ed4f92d021d707f2a0ad6854bf944b
Austin "Felix" Lee <felixpitau@gmail.com>
2018-03-30 23:25:44 -0600

    Update imagine.js

--- talents/imagine.js
+++ talents/imagine.js
@@ -15,20 +15,60 @@ default export class Imagine extends Talent {
     }
   }
 
+  start (m) {
+    this.say(m, 'Starting game! Join up by using `imagineif join`')
+    Imagine.game = {
+      stage: 'person',
+      turnCount: 0,
+      inGame: true,
+      players: [],
+      subjects: []
+    }
+  }
+
+  end (m) {
+    
+  }
+
+  join (m) {
+    
+  }
+
+  leave (m) {
+    
+  }
+
+  subjectAdd (m, subject) {
+    
+  }
+
+  subjectRemove (m, subject) {
+    
+  }
+
+  choose (m, choice) {
+    let roll = () => {return g.subjects[Math.floor(Math.random() * g.subjects.length)]}
+    let rollOne = roll()
+    let rollTwo = roll()
+    until (rollOne !== rollTwo || g.subjects.length < 2) {
+      rollTwo = roll()
+    }
+  }
+
   onMessage (message) {
     let m = message
-    this.react(m, /^\imagineif/gi, () => {
-      if (Imagine.game === null) {
-        Imagine.game = {
-          rolled: false,
-          turnCount: 0,
-          inGame: false,
-          players: [],
-          subjects: []
-        }
+    if (Imagine.game === null) {
+      Imagine.game = {
+        stage: 'person',
+        turnCount: 0,
+        inGame: false,
+        players: [],
+        subjects: []
       }
-      let g = Imagine.game
-      this.react(m, /^imagineif$/gi, 'use: `imagineif (status|roll|choose|join|start|end)`')
+    }
+    let g = Imagine.game
+    this.react(m, /^\imagineif/gi, () => {
+      this.react(m, /^imagineif$/gi, 'use: `imagineif (status|subject|choose|join|start|end)`')
       this.react(m, /^imagineif status$/gi, () => {
         if (g.inGame) {
           this.say(m, 'A game is in progress and it is ' + client.fetchUser(g.turn).username + '\'s turn!')
@@ -36,33 +76,19 @@ default export class Imagine extends Talent {
           this.say(m, 'A game is not currently in progress')
         }
       })
-      this.react(m, /^imagineif roll$/gi, () => {
-        if (g.inGame) {
-          if (this.turn() === m.author) {
-            if (g.rolled === false) {
-              this.say(m, 'Rolling!')
-              g.rolled = true
-              let roll = () => {return g.subjects[Math.floor(Math.random() * g.subjects.length)]}
-              let rollOne = roll()
-              let rollTwo = roll()
-              until (rollOne !== rollTwo || g.subjects.length < 2) {
-                rollTwo = roll()
-              }
-            } else {
-              this.say(m, 'You already rolled! You must now choose!')
-            }
-          } else {
-            this.say(m, 'It is not your turn, it is ' + client.getchUser(g.turn).username + '\'s turn!')
-          }
-        } else {
-          this.say(m, 'There is no game to roll for!')
-        }
-      })
     })
     this.react(m, /^imagineif choose/gi, () => {
       this.react(m, /^imagineif choose$/gi, 'use `imagineif choose (1..6)` to choose a person or a response')
-      this.react(m, /^imagineif choose [0-6]$/gi, () => {
-        
+      this.react(m, /^imagineif choose [0-9]$/gi, () => {
+        if (g.inGame) {
+          if (g.stage === 'person') {
+            
+          } else if (g.stage === 'response') {
+            
+          }
+        } else {
+          this.say(m, 'There is no game running right now')
+        }
       })
     })
     this.react(m, /^imagineif join/gi, () => {
@@ -76,15 +102,48 @@ default export class Imagine extends Talent {
         g.players.push({
           id: m.author.id,
           position: 0,
+          score: 0,
+          inGame: true,
+          choice: false,
           turn: false
         })
+      } else {
+        this.say(m, 'You have already joined!')
       }
     })
+    this.react(m, /^imagineif leave/gi, () => {
+      for (let player in g.players) {
+        if (player.id === m.author.id) {
+          player.inGame = false
+        }
+      }
+    })
+    this.react(m, /^imagineif subject/gi, () => {
+      this.react(m, /^imagineif subject$/gi, 'use: `imagineif subject (add|remove)`')
+    })
     this.react(m, /^imagineif start/gi, () => {
-      
+      this.react(m, /^imagineif start$/gi, () => {
+        if (g.inGame) {
+          this.say(m, 'You already have a game going! do `imagineif start!` to start anyways')
+        } else {
+          this.start(m)
+        }
+      })
+      this.react(m, /^imagineif start\!$/gi, () => {
+        this.start(m)
+      })
     })
     this.react(m, /^imagineif end/gi, () => {
-      
+      this.react(m, /^imagineif end$/gi, () => {
+        if (g.inGame) {
+          this.say(m, 'Are you sure? do `imagineif end!` if you\'re sure')
+        } else {
+          this.say(m, 'There is no game running to end.')
+        }
+      }
+      this.react(m, /^imagineif end\!$/gi, () => {
+        this.end(m)
+      })
     })
   }
 }
